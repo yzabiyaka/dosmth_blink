f5d4fb9579a3071501c3a37c6f1d993df37c51f6
Sergii Tkachenko <hi@sergii.org>
2017-03-09 13:40:13 -0500

    Prepend logs with date timestamp

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const winston = require('winston');
  * Constants declarations.
  */
 const LISTEN_PORT = process.env.LISTEN_PORT || 5050;
-const LOGGING_LEVEL = process.env.LOGGING_LEVEL || 'info';
-
+const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
+const LOGGER_TIMESTAMP = !(process.env.LOGGER_TIMESTAMP === 'false');
 
 /**
  * Dependencies initializations.
@@ -21,7 +21,12 @@ const app = express();
 // Logger:
 app.locals.logger = new winston.Logger({
   transports: [
-    new winston.transports.Console({ prettyPrint: true, colorize: true, level: LOGGING_LEVEL }),
+    new winston.transports.Console({
+      prettyPrint: true,
+      colorize: true,
+      level: LOGGER_LEVEL,
+      timestamp: LOGGER_TIMESTAMP,
+    }),
   ],
   exceptionHandlers: [
     new winston.transports.Console({ prettyPrint: true, colorize: true }),
