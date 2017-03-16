fc63443af6f7ee2faefe38dfe5c800e6bc7796c9
Sergii Tkachenko <hi@sergii.org>
2017-03-16 15:10:08 -0400

    Queue model superclass test

---
+++ test/lib/Queue.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Imports.
+ */
+const test = require('ava');
+const Queue = require('../../lib/Queue');
+
+/**
+ * Test Queue class
+ */
+test('Queue methods', () => {
+  const queue = new Queue();
+  queue.should.respondTo('pub');
+  queue.should.respondTo('sub');
+  queue.should.have.property('routes');
+  queue.routes.should.be.an('array');
+});
