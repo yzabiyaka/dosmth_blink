cbf0eaed780c3818a1dbc183c13c68dc9ab6518e
Sergii Tkachenko <hi@sergii.org>
2017-03-16 15:31:27 -0400

    Code style fixes

--- queues/RemoteHttpRequestQ.js
+++ queues/RemoteHttpRequestQ.js
@@ -3,17 +3,7 @@
 const Queue = require('../lib/Queue');
 
 class RemoteHttpRequestQ extends Queue {
-  constructor() {
-    super();
-  }
 
-  pub() {
-    return false;
-  }
-
-  sub() {
-    return false;
-  }
 }
 
 module.exports = RemoteHttpRequestQ;
