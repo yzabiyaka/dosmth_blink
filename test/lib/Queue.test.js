7350747c367abe8e93da37e9a35ed11fc7ab6d6f
Sergii Tkachenko <hi@sergii.org>
2017-03-17 17:59:37 -0400

    Rename test queue management vars

--- test/lib/Queue.test.js
+++ test/lib/Queue.test.js
@@ -51,21 +51,21 @@ test('Test RabbitMQ topology assertion', async () => {
 
   // Test queue settings with RabbitMQ Management Plugin API.
   const rabbit = new RabbitManagement(locals.amqpManagement);
-  const tacoFactoryInfo = await rabbit.getQueueInfo(tacoRecipesQ);
-  tacoFactoryInfo.should.have.property('name', 'taco-recipes');
-  tacoFactoryInfo.should.have.property('durable', true);
-  tacoFactoryInfo.should.have.property('auto_delete', false);
-  tacoFactoryInfo.should.have.property('exclusive', false);
+  const tacoRecipesQInfo = await rabbit.getQueueInfo(tacoRecipesQ);
+  tacoRecipesQInfo.should.have.property('name', 'taco-recipes');
+  tacoRecipesQInfo.should.have.property('durable', true);
+  tacoRecipesQInfo.should.have.property('auto_delete', false);
+  tacoRecipesQInfo.should.have.property('exclusive', false);
 
   // Test that the queue is binded to the exchange.
-  const tacoFactoryBindings = await rabbit.getQueueBindings(tacoRecipesQ);
-  tacoFactoryBindings.should.be.an('array').and.have.length(2);
+  const tacoRecipesQBindings = await rabbit.getQueueBindings(tacoRecipesQ);
+  tacoRecipesQBindings.should.be.an('array').and.have.length(2);
   // Specific route
-  tacoFactoryBindings[0].should.have.property('routing_key', '*.mexican.food');
-  tacoFactoryBindings[0].should.have.property('source', tacoX.name);
-  tacoFactoryBindings[0].should.have.property('destination', 'taco-recipes');
+  tacoRecipesQBindings[0].should.have.property('routing_key', '*.mexican.food');
+  tacoRecipesQBindings[0].should.have.property('source', tacoX.name);
+  tacoRecipesQBindings[0].should.have.property('destination', 'taco-recipes');
   // Direct route
-  tacoFactoryBindings[1].should.have.property('routing_key', 'taco-recipes');
-  tacoFactoryBindings[1].should.have.property('source', tacoX.name);
-  tacoFactoryBindings[1].should.have.property('destination', 'taco-recipes');
+  tacoRecipesQBindings[1].should.have.property('routing_key', 'taco-recipes');
+  tacoRecipesQBindings[1].should.have.property('source', tacoX.name);
+  tacoRecipesQBindings[1].should.have.property('destination', 'taco-recipes');
 });
