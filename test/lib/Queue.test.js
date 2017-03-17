59831b31b95091d13bd50cd4ac40fb2dd1be2585
Sergii Tkachenko <hi@sergii.org>
2017-03-17 17:23:57 -0400

    Test exchange <=> queue bindings

--- test/lib/Queue.test.js
+++ test/lib/Queue.test.js
@@ -28,25 +28,45 @@ test('Queue superclass', () => {
  * in RabbitMQ.
  */
 test('Test RabbitMQ topology assertion', async () => {
-  class TacoFactoryQ extends Queue {}
+  class TacoRecipesQ extends Queue {
+    constructor(exchange) {
+      super(exchange);
+      this.routes.push('*.mexican.food');
+    }
+  }
 
   const locals = require('../../config');
   const tacoX = new Exchange(locals.amqp);
   await tacoX.setup();
 
-  const tacoFactoryQ = new TacoFactoryQ(tacoX);
-  tacoFactoryQ.should.have.property('name');
-  tacoFactoryQ.name.should.be.equal('taco-factory');
+  const tacoRecipesQ = new TacoRecipesQ(tacoX);
+  tacoRecipesQ.should.have.property('name');
+  tacoRecipesQ.name.should.be.equal('taco-recipes');
+  // Direct + *.mexican.food
+  tacoRecipesQ.routes.length.should.equal(2);
 
-  tacoFactoryQ.should.be.an.instanceof(Queue);
-  const result = await tacoFactoryQ.setup();
+  tacoRecipesQ.should.be.an.instanceof(Queue);
+  const result = await tacoRecipesQ.setup();
   result.should.be.true;
 
   // Test queue settings with RabbitMQ Management Plugin API.
   const rabbit = new RabbitManagement(locals.amqpManagement);
-  const tacoFactoryInfo = await rabbit.getQueueInfo(tacoFactoryQ);
-  tacoFactoryInfo.should.have.property('name', 'taco-factory');
+  const tacoFactoryInfo = await rabbit.getQueueInfo(tacoRecipesQ);
+  tacoFactoryInfo.should.have.property('name', 'taco-recipes');
   tacoFactoryInfo.should.have.property('durable', true);
   tacoFactoryInfo.should.have.property('auto_delete', false);
   tacoFactoryInfo.should.have.property('exclusive', false);
+
+  // Test that the queue is binded to the exchange.
+  const tacoFactoryBindings = await rabbit.getQueueBindings(tacoRecipesQ);
+  tacoFactoryBindings.should.have.length(2);
+  // Specific route
+  tacoFactoryBindings[0].should.have.property('routing_key', '*.mexican.food');
+  tacoFactoryBindings[0].should.have.property('source', tacoX.name);
+  tacoFactoryBindings[0].should.have.property('destination', 'taco-recipes');
+  // Direct route
+  tacoFactoryBindings[1].should.have.property('routing_key', 'taco-recipes');
+  tacoFactoryBindings[1].should.have.property('source', tacoX.name);
+  tacoFactoryBindings[1].should.have.property('destination', 'taco-recipes');
+
 });
