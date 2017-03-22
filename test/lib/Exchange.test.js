98d6908d669b2e1fe0af72cbe716b996a872ca11
Sergii Tkachenko <hi@sergii.org>
2017-03-22 13:50:54 -0400

    Test failed Queue assertion

--- test/lib/Exchange.test.js
+++ test/lib/Exchange.test.js
@@ -7,6 +7,8 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const test = require('ava');
 const Exchange = require('../../lib/Exchange');
+const Queue = require('../../lib/Queue');
+
 
 // Chai setup.
 chai.should();
@@ -48,6 +50,31 @@ test('Exchange::setup() should fail with empty exchange name', async () => {
   // No exchange name should result in rejection from Exchange::setup() .
   await incorrectSetupResult.should.be.rejectedWith(
     Error,
-    'Incorrect Exchange::setup() response for Exchange ""'
+    'Exchange.setup(): Exchange assertion failed for ""'
+  );
+});
+
+
+/**
+ * Exchange::assertQueue() should fail with incorrect queueu name
+ */
+test('Exchange::assertQueue() should fail with empty exchange name', async () => {
+  class WrongNameQ extends Queue {}
+
+  const locals = require('../../config');
+  const tacoX = new Exchange(locals.amqp);
+  await tacoX.setup();
+
+  // Fake queue not initialized in Rabbit.
+  const wrongNameQ = new WrongNameQ(tacoX);
+  // Override Queue name and watch setup fail.
+  // Empty string would result in automatically generated queue name,
+  // thus Queue.setup() method will fail.
+  wrongNameQ.name = '';
+
+  const incorrectSetupResult = wrongNameQ.setup();
+  await incorrectSetupResult.should.be.rejectedWith(
+    Error,
+    'Exchange.setup(): Queue assertion failed for ""'
   );
 });
