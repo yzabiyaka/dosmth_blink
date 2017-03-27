4ddbea60d4f09415ea394280f522bc1434c25dfa
Sergii Tkachenko <hi@sergii.org>
2017-03-27 11:49:09 -0400

    Refactor web app configuration

--- config/index.js
+++ config/index.js
@@ -3,22 +3,23 @@
 const fs = require('fs');
 
 // Settings.
-const locals = {};
+const config = {};
 
 // Logger
-locals.express = require('./express');
-locals.logger = require('./logger');
-locals.amqp = require('./amqp');
-locals.amqpManagement = require('./amqpManagement');
+config.app = require('./app');
+config.logger = require('./logger');
+config.express = require('./express');
+config.amqp = require('./amqp');
+config.amqpManagement = require('./amqpManagement');
 
 // Require env-dependent configs
-const envConfigPath = `${__dirname}/env/override-${locals.express.env}.js`;
+const envConfigPath = `${__dirname}/env/override-${config.app.env}.js`;
 const stats = fs.lstatSync(envConfigPath);
 if (stats.isFile()) {
   // Apply environment overrides.
   // We really want dynamic require here, so forcing eslint to ignore next line.
   // eslint-disable-next-line import/no-dynamic-require, global-require
-  require(envConfigPath)(locals);
+  require(envConfigPath)(config);
 }
 
-module.exports = locals;
+module.exports = config;
