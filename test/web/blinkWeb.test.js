ab4eefdfb26938a616332a44ccbcdc3216f26608
Sergii Tkachenko <hi@sergii.org>
2017-04-06 16:31:09 -0400

    Now with 100% more Doctor Who easter eggs

--- test/web/blinkWeb.test.js
+++ test/web/blinkWeb.test.js
@@ -9,7 +9,17 @@ require('chai').should();
 const blinkWeb = require('../../web/blinkWeb');
 
 /**
- * Test root.
+ * Test GET / with no auth
+ */
+test('GET / should turn down anonymous requests', async () => {
+  const res = await supertest(blinkWeb.callback()).get('/');
+  res.status.should.be.equal(401);
+  res.text.should.be.have.string('Don\'t blink');
+});
+
+
+/**
+ * Test GET /.
  */
 test('GET / should be polite', async () => {
   const config = require('../../config');
