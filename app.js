9068d73d499b03d9d32ae7bf99fed28b74676ed5
Sergii Tkachenko <hi@sergii.org>
2017-04-11 14:20:56 -0400

    Make Blink class more testable

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 'use strict';
 
+// ------- Imports -------------------------------------------------------------
+
 const Blink = require('./src/Blink.js');
 const config = require('./config');
 
-Blink.run(config);
+// ------- Args parse ----------------------------------------------------------
+
+// TODO: Arg parse
+const bootstrapLevel = process.argv[2];
+const workerName = process.argv[3];
+
+// ------- Bootstrap -----------------------------------------------------------
+
+const blink = new Blink(config);
+
+switch (bootstrapLevel) {
+  case 'web':
+    blink.bootstrapWeb();
+    break;
+  case 'worker':
+    blink.bootstrapWorker(workerName);
+    break;
+  default:
+    // Shouldn't happen
+}
--- src/Blink.js
+++ src/Blink.js
@@ -5,43 +5,21 @@ class Blink {
     this.config = config;
   }
 
-  bootstrap() {
-    // TODO: Parse args.
-    const bootstrapLevel = process.argv[2];
-    const workerName = process.argv[3];
-
-    this.bootstrapBase();
-
-    switch (bootstrapLevel) {
-      case 'web':
-        this.bootstrapWeb();
-        break;
-      case 'worker':
-        this.bootstrapWorker(workerName);
-        break;
-      default:
-        // error
-    }
-  }
-
-  bootstrapBase() {
+  initialSetup() {
     // TODO: create exchange and queueus
-    this.config.logger.info('bootstrapBase');
+    this.config.logger.info('initialSetup');
   }
 
   bootstrapWeb() {
+    this.initialSetup();
     // TODO: start web
     this.config.logger.info('bootstrapWeb');
   }
 
-  bootstrapWorker() {
+  bootstrapWorker(name) {
+    this.initialSetup();
     // TODO: run worker
-    this.config.logger.info('bootstrapWorker');
-  }
-
-  static run(config) {
-    const blink = new Blink(config);
-    blink.bootstrap();
+    this.config.logger.info(`bootstrapWorker: ${name}`);
   }
 }
 
