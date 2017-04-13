06e60178de67d3294e412d6540713705c9b71f05
Sergii Tkachenko <hi@sergii.org>
2017-04-13 13:45:21 -0400

    Move old consume message to concrete class an example

--- src/workers/FetchWorker.js
+++ src/workers/FetchWorker.js
@@ -27,6 +27,33 @@ class FetchWorker extends Worker {
     }
     return false;
   }
+
+  // TODO: better logging
+  // async consume(rawMessage) {
+  //   try {
+  //     // Will throw MessageValidationBlinkError when not valid.
+  //     // const message = this.queue.validateIncomingMessage(incomingMessage);
+  //     // TODO: print message metadata
+  //     this.logger.info(`Message valid | ${validMessage.payload.meta.id}`);
+  //     const processResult = await this.process(validMessage);
+  //     if (processResult) {
+  //       this.logger.info(`Message processed | ${validMessage.payload.meta.id}`);
+  //     } else {
+  //       this.logger.info(`Message not processed | ${validMessage.payload.meta.id}`);
+  //     }
+  //   } catch (error) {
+  //     if (error instanceof MessageParsingBlinkError) {
+  //       this.logger.error(`Queue ${this.name}: can't parse payload, reason: "${error}", payload: "${error.rawPayload}"`);
+  //     } else if (error instanceof MessageValidationBlinkError) {
+  //       this.logger.error(`Queue ${this.name}: message validation error: "${error}", payload: "${error.payload}"`);
+  //     } else {
+  //       this.logger.error(`Queue ${this.name} uncaught exception ${error}`);
+  //     }
+
+  //     // TODO: send to dead letters?
+  //     this.queue.nack(incomingMessage);
+  //   }
+  // }
 }
 
 module.exports = FetchWorker;
