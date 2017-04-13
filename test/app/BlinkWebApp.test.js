9e2121607f252a8427ece89543ee7d83f9581594
Sergii Tkachenko <hi@sergii.org>
2017-04-13 16:43:47 -0400

    Move blinkWeb test to app/BlinkWebApp.test.js

---
+++ test/app/BlinkWebApp.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// ------- Imports -------------------------------------------------------------
+
+const test = require('ava');
+const chai = require('chai');
+
+// ------- Init ----------------------------------------------------------------
+
+chai.should();
+
+// ------- Tests ---------------------------------------------------------------
+
+/**
+ * GET /
+ */
+test.skip('GET / should turn down anonymous requests', async () => {
+  const res = await supertest(blinkWeb.callback()).get('/');
+  res.status.should.be.equal(401);
+  res.text.should.be.have.string('Don\'t blink');
+});
+
+// ------- End -----------------------------------------------------------------
