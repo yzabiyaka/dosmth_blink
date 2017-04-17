73c3250b3ae73aae3a0be33a8765299398a7bb68
Sergii Tkachenko <hi@sergii.org>
2017-04-17 15:41:27 -0400

    Make logger colorization and pretty printing configurable

--- config/logger.js
+++ config/logger.js
@@ -5,17 +5,22 @@ const winston = require('winston');
 // Setup winston default instance
 const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
 const LOGGER_TIMESTAMP = !(process.env.LOGGER_TIMESTAMP === 'false');
+const LOGGER_COLORIZE = process.env.LOGGER_COLORIZE === 'true'
+const LOGGER_PRETTY_PRINT = process.env.LOGGER_PRETTY_PRINT === 'true';
 
 winston.configure({
   transports: [
     new winston.transports.Console({
-      prettyPrint: true,
-      colorize: true,
+      prettyPrint: LOGGER_PRETTY_PRINT,
+      colorize: LOGGER_COLORIZE,
       level: LOGGER_LEVEL,
       timestamp: LOGGER_TIMESTAMP,
     }),
   ],
   exceptionHandlers: [
-    new winston.transports.Console({ prettyPrint: true, colorize: true }),
+    new winston.transports.Console({
+      prettyPrint: LOGGER_PRETTY_PRINT,
+      colorize: LOGGER_COLORIZE,
+    }),
   ],
 });
