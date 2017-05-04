9eb7933e0dfad604d0008d7964f80459d181d0c9
Sergii Tkachenko <hi@sergii.org>
2017-05-04 14:46:49 -0400

    Create CustomerIoUpdateCustomerWorker

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: npm run web
 fetch: npm run worker fetch
 gambit-chatbot-mdata-proxy: npm run worker gambit-chatbot-mdata-proxy
+customer-io-update-customer: npm run worker customer-io-update-customer
--- src/messages/UserMessage.js
+++ src/messages/UserMessage.js
@@ -3,6 +3,7 @@
 const Joi = require('joi');
 
 const Message = require('./Message');
+const MessageParsingBlinkError = require('../errors/MessageParsingBlinkError');
 
 class UserMessage extends Message {
 
@@ -66,6 +67,22 @@ class UserMessage extends Message {
         request_id: ctx.id,
       },
     });
+    return userMessage;
+  }
+
+  static fromRabbitMessage(rabbitMessage) {
+    const payload = this.parseIncomingPayload(rabbitMessage);
+    if (!payload.data || !payload.meta) {
+      throw new MessageParsingBlinkError('No data in message', payload);
+    }
+
+    // TODO: save more metadata
+    // TODO: metadata parse helper
+    const userMessage = new UserMessage({
+      data: payload.data,
+      meta: payload.meta,
+    });
+    userMessage.fields = rabbitMessage.fields;
     return userMessage;
   }
 
