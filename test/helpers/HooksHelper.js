cba87540f599d4e21484cef1807b6274a426637b
Rafa <rapala61@gmail.com>
2017-05-09 15:20:09 -0400

    adds more randomized attributes to the output of buildValidUserMessage

--- test/helpers/HooksHelper.js
+++ test/helpers/HooksHelper.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// libraries
 const supertest = require('supertest');
+const faker = require('faker');
+const underscore = require('underscore');
 
+// App modules
 const BlinkWebApp = require('../../src/app/BlinkWebApp');
 const UserMessage = require('../../src/messages/UserMessage');
-const faker = require('faker');
 
 class HooksHelper {
 
@@ -24,6 +27,9 @@ class HooksHelper {
   static buildValidUserMessage(t) {
     const bDay = faker.date.past();
     const fakeId = `57d1aa6142a06${Date.now().toString(16)}`;
+    const roles = ['user', 'admin', 'staff'];
+    const sources = ['niche', 'phoenix', 'after_school'];
+    const mobileStatuses = ['undeliverable', 'active', 'unknown'];
 
     t.context.user = new UserMessage({
       data: {
@@ -42,20 +48,20 @@ class HooksHelper {
         ],
         birthdate: `${bDay.getFullYear()}-${bDay.getMonth()}-${bDay.getDate()}`,
         addr_street1: faker.address.streetAddress(),
-        addr_street2: null,
+        addr_street2: faker.address.secondaryAddress(),
         addr_city: faker.address.city(3),
         addr_state: faker.address.state(),
         addr_zip: faker.address.zipCode('#####'),
-        source: 'phoenix',
-        source_detail: null,
+        source: underscore.sample(sources),
+        source_detail: faker.random.word(),
         slack_id: faker.random.number(4091040),
         mobilecommons_id: faker.random.number(167181555),
         parse_installation_ids: null,
-        mobile_status: 'undeliverable',
-        language: 'en',
+        mobile_status: underscore.sample(mobileStatuses),
+        language: faker.random.locale(),
         country: faker.address.countryCode(),
         drupal_id: faker.random.number(4091040),
-        role: 'user',
+        role: underscore.sample(roles),
         last_authenticated_at: faker.date.past(),
         updated_at: faker.date.past(),
         created_at: faker.date.past(),
