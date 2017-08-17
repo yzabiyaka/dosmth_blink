5bf60e3c0faeab43d3ad121589a69bf768776fc2
Sergii Tkachenko <hi@sergii.org>
2017-08-17 13:38:08 -0400

    Start testing gambit-message-data-relay

--- test/workers/GambitMessageDataRelayWorker.test.js
+++ test/workers/GambitMessageDataRelayWorker.test.js
@@ -16,27 +16,27 @@ const { Response } = fetch;
 
 // ------- Tests ---------------------------------------------------------------
 
-// test('Gambit should recieve correct retry count if message has been retried', () => {
-//   const config = require('../../config');
-//   const gambitWorkerApp = new BlinkWorkerApp(config, 'gambit-chatbot-mdata-proxy');
-//   const gambitWorker = gambitWorkerApp.worker;
+test('Gambit should recieve correct retry count if message has been retried', () => {
+  const config = require('../../config');
+  const gambitWorkerApp = new BlinkWorkerApp(config, 'gambit-message-data-relay');
+  const gambitWorker = gambitWorkerApp.worker;
 
-//   // No retry property:
-//   gambitWorker.getRequestHeaders(MessageFactoryHelper.getValidMdata())
-//     .should.not.have.property('x-blink-retry-count');
+  // No retry property:
+  gambitWorker.getRequestHeaders(MessageFactoryHelper.getValidMdata())
+    .should.not.have.property('x-blink-retry-count');
 
-//   // retry = 0
-//   const retriedZero = MessageFactoryHelper.getValidMdata();
-//   retriedZero.payload.meta.retry = 0;
-//   gambitWorker.getRequestHeaders(retriedZero)
-//     .should.not.have.property('x-blink-retry-count');
+  // retry = 0
+  const retriedZero = MessageFactoryHelper.getValidMdata();
+  retriedZero.payload.meta.retry = 0;
+  gambitWorker.getRequestHeaders(retriedZero)
+    .should.not.have.property('x-blink-retry-count');
 
-//   // retry = 1
-//   const retriedOnce = MessageFactoryHelper.getValidMdata();
-//   retriedOnce.payload.meta.retry = 1;
-//   gambitWorker.getRequestHeaders(retriedOnce)
-//     .should.have.property('x-blink-retry-count').and.equal(1);
-// });
+  // retry = 1
+  const retriedOnce = MessageFactoryHelper.getValidMdata();
+  retriedOnce.payload.meta.retry = 1;
+  gambitWorker.getRequestHeaders(retriedOnce)
+    .should.have.property('x-blink-retry-count').and.equal(1);
+});
 
 
 // test('Test Gambit response with x-blink-retry-suppress header', () => {
