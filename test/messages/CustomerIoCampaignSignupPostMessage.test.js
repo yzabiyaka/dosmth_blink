45f2fc972b2bc37d3e1748240ea866e969a9336f
Sergii Tkachenko <hi@sergii.org>
2017-09-19 12:59:01 -0400

    Strat testing signup post -> cio transformation

--- test/messages/CustomerIoCampaignSignupPostMessage.test.js
+++ test/messages/CustomerIoCampaignSignupPostMessage.test.js
@@ -7,23 +7,53 @@ const chai = require('chai');
 // const moment = require('moment');
 
 const CustomerIoCampaignSignupPostMessage = require('../../src/messages/CustomerIoCampaignSignupPostMessage');
-// const CustomerIoEvent = require('../../src/models/CustomerIoEvent');
+const CustomerIoEvent = require('../../src/models/CustomerIoEvent');
 const MessageFactoryHelper = require('../helpers/MessageFactoryHelper');
 
 // ------- Init ----------------------------------------------------------------
 
 chai.should();
-// const expect = chai.expect;
+const expect = chai.expect;
 const generator = MessageFactoryHelper.getValidCampaignSignupPost;
 
 // ------- Tests ---------------------------------------------------------------
 
-test('Campaign signup event message generator', () => {
+test('Campaign signup post message generator', () => {
   generator().should.be.an.instanceof(CustomerIoCampaignSignupPostMessage);
 });
 
-test('Campaign signup message should have toCustomerIoEvent', () => {
+test('Campaign signup post message should have toCustomerIoEvent', () => {
   generator().should.respondsTo('toCustomerIoEvent');
+});
+
+test('Campaign signup post message should be correctly transformed to CustomerIoEvent', () => {
+  let count = 100;
+  while (count > 0) {
+    const msg = generator();
+    const data = msg.getData();
+    const cioEvent = msg.toCustomerIoEvent();
+
+    expect(cioEvent).to.be.an.instanceof(CustomerIoEvent);
+
+    cioEvent.getId().should.equal(data.northstar_id);
+    cioEvent.getName().should.equal('campaign_signup_post');
+
+    // const eventData = cioEvent.getData();
+
+    // eventData.signup_id.should.equal(String(data.id));
+    // eventData.campaign_id.should.equal(data.campaign_id);
+    // eventData.campaign_run_id.should.equal(data.campaign_run_id);
+
+    // // Todo: make sure TZ is corrected
+    // const originalCreatedAt = moment(data.created_at).milliseconds(0);
+    // const eventCreatedAt = moment.unix(eventData.created_at);
+    // eventCreatedAt.toISOString().should.be.equal(originalCreatedAt.toISOString());
+
+    // if (data.source) {
+    //   expect(eventData.source).to.be.equal(data.source);
+    // }
+    count -= 1;
+  }
 });
 
 // ------- End -----------------------------------------------------------------
