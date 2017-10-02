f42df46279d9f1efdb69ea31e8b783749697dc31
Sergii Tkachenko <hi@sergii.org>
2017-10-02 15:13:16 -0400

    Add nase test for twilio-sms-inbound webhook

--- test/web/controllers/WebHooksWebController.test.js
+++ test/web/controllers/WebHooksWebController.test.js
@@ -45,6 +45,9 @@ test('GET /api/v1/webhooks should respond with JSON list available webhooks', as
 
   res.body.should.have.property('twilio-sms-broadcast')
     .and.have.string('/api/v1/webhooks/twilio-sms-broadcast');
+
+  res.body.should.have.property('twilio-sms-inbound')
+    .and.have.string('/api/v1/webhooks/twilio-sms-inbound');
 });
 
 /**
@@ -263,6 +266,45 @@ test('POST /api/v1/webhooks/twilio-sms-broadcast should publish message to twili
   messageData.should.have.property('meta');
   messageData.meta.should.have.property('query');
   messageData.meta.query.should.have.property('broadcastId', broadcastId);
+});
+
+/**
+ * POST /api/v1/webhooks/twilio-sms-inbound
+ */
+test('POST /api/v1/webhooks/twilio-sms-inbound should publish message to twilio-sms-inbound-gambit-relay queue', async (t) => {
+  const data = {
+    random: 'key',
+    nested: {
+      random2: 'key2',
+    },
+  };
+
+  const broadcastId = chance.word();
+
+  const res = await t.context.supertest.post('/api/v1/webhooks/twilio-sms-inbound')
+    .query({ broadcastId })
+    .auth(t.context.config.app.auth.name, t.context.config.app.auth.password)
+    .send(data);
+
+  res.status.should.be.equal(202);
+
+  // Check response to be json
+  res.header.should.have.property('content-type');
+  res.header['content-type'].should.match(/json/);
+
+  // Check response.
+  res.body.should.have.property('ok', true);
+
+  // Check that the message is queued.
+  const rabbit = new RabbitManagement(t.context.config.amqpManagement);
+  const messages = await rabbit.getMessagesFrom('twilio-sms-inbound-gambit-relay', 1, false);
+  messages.should.be.an('array').and.to.have.lengthOf(1);
+
+  messages[0].should.have.property('payload');
+  const payload = messages[0].payload;
+  const messageData = JSON.parse(payload);
+  messageData.should.have.property('data');
+  messageData.data.should.be.eql(data);
 });
 
 // ------- End -----------------------------------------------------------------
