717ca0a21f5db7ab0b7d524e9ca4668a8b774e0a
Sergii Tkachenko <hi@sergii.org>
2017-10-23 17:49:20 -0400

    Test RetryManager.retry() logic

--- test/lib/RetryManager.test.js
+++ test/lib/RetryManager.test.js
@@ -55,7 +55,7 @@ test('RetryManager.retry(): ensure nack when retry limit is reached', (t) => {
   const nackStub = sinon.stub(queue, 'nack').returns(null);
 
   // Create retryManager.
-  const retryManager = new RetryManager(t.context.queue);
+  const retryManager = new RetryManager(queue);
 
   // Prepare retry message for the manager.
   const message = MessageFactoryHelper.getRandomMessage();
@@ -74,6 +74,42 @@ test('RetryManager.retry(): ensure nack when retry limit is reached', (t) => {
   // Cleanup.
   ackStub.restore();
   nackStub.restore();
+});
+
+/**
+ * RetryManager.retry()
+ */
+test('RetryManager.retry(): should call scheduleRedeliveryIn with correct params', (t) => {
+  const queue = t.context.queue;
+
+  // Create retryManager.
+  const retryManager = new RetryManager(queue);
+
+  // Prepare retry message for the manager.
+  const message = MessageFactoryHelper.getRandomMessage();
+  const retryError = new BlinkRetryError('Testing BlinkRetryError', message);
+  // Set current retry attempt to the limit set in the manager - 1.
+  const retryAttempt = retryManager.retryLimit - 1;
+  message.payload.meta.retryAttempt = retryManager.retryLimit - 1;
+
+  // Stub scheduleRedeliveryIn.
+  const scheduleRedeliveryInStub = sinon.stub(retryManager, 'scheduleRedeliveryIn');
+  scheduleRedeliveryInStub.returns(null);
+
+  // Pass the message to retry().
+  const result = retryManager.retry(message, retryError);
+  result.should.be.true;
+
+  scheduleRedeliveryInStub.should.have.been.calledWith(
+    DelayLogic.exponentialBackoff(retryAttempt),
+    message,
+  );
+
+  // Ensure message has correct retryAttempt setting.
+  message.getMeta().retryAttempt.should.equal(retryAttempt);
+
+  // Cleanup.
+  scheduleRedeliveryInStub.restore();
 });
 
 // ------- End -----------------------------------------------------------------
