f4c3753db70b81dda727bbda98f60f4ef487b38a
Sergii Tkachenko <hi@sergii.org>
2017-10-23 17:24:12 -0400

    Ensure is's possible to override delay logic in RetryManager

--- test/lib/RetryManager.test.js
+++ test/lib/RetryManager.test.js
@@ -9,6 +9,7 @@ const chai = require('chai');
 
 // const BlinkRetryError = require('../../src/errors/BlinkRetryError');
 const RetryManager = require('../../src/lib/RetryManager');
+const DelayLogic = require('../../src/lib/DelayLogic');
 // const FreeFormMessage = require('../../src/messages/FreeFormMessage');
 const HooksHelper = require('../helpers/HooksHelper');
 // const MessageFactoryHelper = require('../helpers/MessageFactoryHelper');
@@ -35,4 +36,12 @@ test('RetryManager: Test class interface', (t) => {
   retryManager.should.respondTo('redeliver');
   retryManager.should.respondTo('log');
   retryManager.should.have.property('retryLimit');
+  // Ensure default retry delay logic is DelayLogic.exponentialBackoff
+  retryManager.retryDelay.should.be.equal(DelayLogic.exponentialBackoff);
+
+  // Ensure it's possible to override DelayLogic
+  const customDelayLogic = currentRetryNumber => currentRetryNumber;
+  const retryManagerCustom = new RetryManager(t.context.queue, customDelayLogic);
+  retryManagerCustom.should.respondTo('retryDelay');
+  retryManagerCustom.retryDelay.should.be.equal(customDelayLogic);
 });
