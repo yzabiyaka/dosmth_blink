0097f87fb296fdf3f050733eb86554dc20d49dc7
Sergii Tkachenko <hi@sergii.org>
2017-11-22 11:31:07 -0500

    Move Queue: Test class interface to unit tests

--- test/integration/lib/Queue.test.js
+++ test/integration/lib/Queue.test.js
@@ -23,23 +23,6 @@ test.afterEach.always(HooksHelper.stopBlinkApp);
 // ------- Tests ---------------------------------------------------------------
 
 /**
- * Queue: Test class interface
- */
-test('Queue: Test class interface', (t) => {
-  const queue = new Queue(t.context.blink.broker);
-  queue.should.respondTo('create');
-  queue.should.respondTo('publish');
-  queue.should.respondTo('nack');
-  queue.should.respondTo('ack');
-  queue.should.respondTo('purge');
-  queue.should.respondTo('delete');
-  queue.should.respondTo('subscribe');
-
-  queue.should.have.property('routes');
-  queue.routes.should.be.an('array').and.have.length.at.least(1);
-});
-
-/**
  * Test that concrete Queue implementation would result in expected queues
  * in RabbitMQ.
  */
