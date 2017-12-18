04690cb4273037ae5945808f1068d2820e0c8c2e
Sergii Tkachenko <hi@sergii.org>
2017-12-18 17:07:19 -0500

    Dust lints from RetryDelayer

--- src/lib/delayers/RetryDelayer.js
+++ src/lib/delayers/RetryDelayer.js
@@ -3,13 +3,27 @@
 // ------- Interface -----------------------------------------------------------
 
 class RetryDelayer {
+  /* eslint-disable no-unused-vars, class-methods-use-this */
+  /* Next methods are interface methods declarations */
+
+  /**
+   * Schedules delay of `delayMs` milliseconds after which the message
+   * with content `messageContent` will be redelivered to the queue `queueName`.
+   *
+   * @param  {string}  queueName      The name of the queue to return the message to
+   * @param  {string}  messageContent The message content, serialized to a string
+   * @param  {integer} delayMs        The delay, milliseconds
+   * @return {boolean}                The result of the operation
+   */
   async retryAfterDelay(queueName, messageContent, delayMs) {
     throw new TypeError('retryAfterDelay() method must be implemented when extending from RetryDelayer');
   }
+
+  /* eslint-enable */
 }
 
 // ------- Exports -------------------------------------------------------------
 
-module.exports = Delayer;
+module.exports = RetryDelayer;
 
 // ------- End -----------------------------------------------------------------
