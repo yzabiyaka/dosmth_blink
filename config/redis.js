79e2eabdd6cb6765287eaf312943a256cf347748
Sergii Tkachenko <hi@sergii.org>
2017-12-18 21:14:29 -0500

    Configure redis retry strategy. Isn't this nice

--- config/redis.js
+++ config/redis.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const DelayLogic = require('../src/lib/delayers/DelayLogic');
+
 const config = {
   host: process.env.BLINK_REDIS_HOST || 'localhost',
   port: process.env.BLINK_REDIS_PORT || '6379',
+  retryStrategy: DelayLogic.constantTimeDelay(1000),
 };
 
 module.exports = config;
