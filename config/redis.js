6a291682ba799bdf46779290812472b584f966b9
Sergii Tkachenko <hi@sergii.org>
2017-12-24 22:34:56 -0500

    User/password support for Redis

--- config/redis.js
+++ config/redis.js
@@ -4,6 +4,10 @@ const config = {
   connection: {
     host: process.env.BLINK_REDIS_HOST || 'localhost',
     port: process.env.BLINK_REDIS_PORT || '6379',
+    options: {
+      user: process.env.BLINK_REDIS_USER || 'redis',
+      password: process.env.BLINK_REDIS_PASSWORD || '',
+    },
   },
   settings: {
     retrySet: process.env.BLINK_REDIS_RETRY_SET_NAME || 'blink_retries',
