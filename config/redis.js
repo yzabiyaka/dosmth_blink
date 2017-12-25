d7d4326c03ed6075a5fb9a129b8b27fababdfbf2
Sergii Tkachenko <hi@sergii.org>
2017-12-24 22:47:21 -0500

    Fix redis AUTH

--- config/redis.js
+++ config/redis.js
@@ -4,10 +4,8 @@ const config = {
   connection: {
     host: process.env.BLINK_REDIS_HOST || 'localhost',
     port: process.env.BLINK_REDIS_PORT || '6379',
-    options: {
-      user: process.env.BLINK_REDIS_USER || 'redis',
-      password: process.env.BLINK_REDIS_PASSWORD || '',
-    },
+    user: process.env.BLINK_REDIS_USER || 'redis',
+    password: process.env.BLINK_REDIS_PASSWORD || '',
   },
   settings: {
     retrySet: process.env.BLINK_REDIS_RETRY_SET_NAME || 'blink_retries',
