b08d548deb4939aca08617db0e59b006ba82b422
Rafa <rapala61@gmail.com>
2018-02-12 14:44:09 -0500

    Override validate method inside FreeFormMessage

--- src/messages/FreeFormMessage.js
+++ src/messages/FreeFormMessage.js
@@ -3,6 +3,7 @@
 const Joi = require('joi');
 
 const Message = require('./Message');
+const MessageValidationBlinkError = require('../errors/MessageValidationBlinkError');
 
 class FreeFormMessage extends Message {
   constructor(...args) {
@@ -11,6 +12,19 @@ class FreeFormMessage extends Message {
     this.schema = Joi.object()
       // Allow presence of all other keys.
       .unknown();
+  }
+
+  validate() {
+    const { error, value } = Joi.validate(
+      this.getData(),
+      this.schema || {},
+    );
+    if (error) {
+      throw new MessageValidationBlinkError(error.message, this.toString());
+    }
+
+    this.payload.data = value;
+    return true;
   }
 }
 
