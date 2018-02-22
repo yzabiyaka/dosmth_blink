37d39eed01e2fb1e9f885c3590667ce14dad243b
Rafa <rapala61@gmail.com>
2018-02-22 10:37:15 -0500

    added tests

---
+++ test/unit/messages/CampaignSignupPostReviewMessage.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// ------- Imports -------------------------------------------------------------
+
+const test = require('ava');
+const chai = require('chai');
+
+const CampaignSignupPostReviewMessage = require('../../../src/messages/CampaignSignupPostReviewMessage');
+const MessageFactoryHelper = require('../../helpers/MessageFactoryHelper');
+
+// ------- Init ----------------------------------------------------------------
+
+chai.should();
+const generator = MessageFactoryHelper.getValidCampaignSignupPostReview;
+
+// ------- Tests ---------------------------------------------------------------
+
+test('Campaign signup post message generator', () => {
+  generator().should.be.an.instanceof(CampaignSignupPostReviewMessage);
+});
+
+test('Campaign signup post message should have toCustomerIoEvent', () => {
+  generator().should.respondsTo('toCustomerIoEvent');
+});
+
+// ------- End -----------------------------------------------------------------
