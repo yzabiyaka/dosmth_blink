43099ddede9e37ceb8ecf389ad5973b2608977f1
Rafa <rapala61@gmail.com>
2018-05-24 09:35:21 -0500

    remove email or mobile validation in user-create

--- src/validations/userCreateAndUpdate.js
+++ src/validations/userCreateAndUpdate.js
@@ -10,16 +10,10 @@ const schema = Joi.object().keys({
   updated_at: Joi.required().empty(whenNullOrEmpty),
   created_at: Joi.required().empty(whenNullOrEmpty),
 
-  // Remove field when provided as empty string or null.
-  email: Joi.string().empty(whenNullOrEmpty).default(undefined),
-  mobile: Joi.string().empty(whenNullOrEmpty).default(undefined),
-
   // Although this is NOT required to be sent, we make it implicitly required to exist in
   // the payload we send to C.io by declaring a default.
   // Allow anything as a role, but default to user.
   role: Joi.string().empty(whenNullOrEmpty).default('user'),
-})
-  // Require presence at least one of: email, mobile.
-  .or('email', 'mobile');
+});
 
 module.exports = schema;
