8577ca3b8b3e0ea918413c34bc65d19715885a23
Rafa <rapala61@gmail.com>
2018-07-27 12:02:27 -0500

    parse REDIS_URL into tokenized REDIS ENV variables

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const { URL } = require('url');
+
+const redisUrl = process.env.REDIS_URL || 'redis://redis@localhost:6379';
+// @see https://nodejs.org/docs/latest-v8.x/api/url.html#url_the_whatwg_url_api
+const parsedRedisUrl = new URL(redisUrl);
+
 const config = {
   connection: {
-    host: process.env.BLINK_REDIS_HOST || 'localhost',
-    port: process.env.BLINK_REDIS_PORT || '6379',
-    user: process.env.BLINK_REDIS_USER || 'redis',
-    password: process.env.BLINK_REDIS_PASSWORD || '',
+    host: process.env.BLINK_REDIS_HOST || parsedRedisUrl.hostname,
+    port: process.env.BLINK_REDIS_PORT || parsedRedisUrl.port,
+    user: process.env.BLINK_REDIS_USER || parsedRedisUrl.username,
+    password: process.env.BLINK_REDIS_PASSWORD || parsedRedisUrl.password,
   },
   settings: {
     retrySet: process.env.BLINK_REDIS_RETRY_SET_NAME || 'blink_retries',
