c437eda6552d3d1dc04b2bf10435f3dbc12ba23b
Rafa <rapala61@gmail.com>
2018-08-17 11:02:59 -0500

    added tests for unsubscribed property

--- test/unit/messages/CustomerIoUpdateCustomerMessage.test.js
+++ test/unit/messages/CustomerIoUpdateCustomerMessage.test.js
@@ -40,6 +40,49 @@ test('Cio identify created from Northstar is correct', () => {
     cioUpdateData.should.have.property('data').and.to.be.an('object');
     cioUpdateData.data.should.have.property('email', userData.email);
     cioUpdateData.data.should.have.property('phone', userData.mobile);
+    /**
+     * The valid user factory sets updatedAt and createdAt to the same date, hence automatically
+     * making the user subscribed
+     * NOTE: If the payload includes an "unsubscribed" property, that takes precedence.
+     */
+    cioUpdateData.data.should.have.property('unsubscribed', false);
+
+    const cioUpdateAttributes = cioUpdateData.data;
+    cioUpdateAttributes.should.have.property(
+      'updated_at',
+      moment(userData.updated_at).unix(),
+    );
+    cioUpdateAttributes.should.have.property(
+      'created_at',
+      moment(userData.created_at).unix(),
+    );
+    count -= 1;
+  }
+});
+
+test('Cio identify created from Northstar is correct when unsubscribed is set', () => {
+  let count = 100;
+  while (count > 0) {
+    const shouldSetUnsubscribed = count % 2 === 0;
+    const userMessage = MessageFactoryHelper.getValidUser();
+    userMessage.validate();
+    // Set unsubscribed
+    userMessage.payload.data.unsubscribed = shouldSetUnsubscribed;
+    const userData = userMessage.getData();
+    const customerIoUpdateCustomerMessage = CustomerIoUpdateCustomerMessage.fromUser(
+      userMessage,
+    );
+
+    const cioUpdateData = customerIoUpdateCustomerMessage.getData();
+
+    // Compare properties.
+
+    // Required:
+    cioUpdateData.should.have.property('id', userData.id);
+    cioUpdateData.should.have.property('data').and.to.be.an('object');
+    cioUpdateData.data.should.have.property('email', userData.email);
+    cioUpdateData.data.should.have.property('phone', userData.mobile);
+    cioUpdateData.data.should.have.property('unsubscribed', userData.unsubscribed);
 
     const cioUpdateAttributes = cioUpdateData.data;
     cioUpdateAttributes.should.have.property(
