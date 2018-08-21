c9e5e7d35858b275c4634089add856ebc269c1c3
Rafa <rapala61@gmail.com>
2018-08-21 11:13:37 -0500

    added events configs for CustomerIoWebhookMessage so that we can route specific c.io events to additional queues than the generic *.event.quasar queue

---
+++ config/messages/CustomerIoWebhookMessage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const config = {};
+
+
+/**
+ * Keys should match event names in C.io.
+ *
+ * @see https://customer.io/docs/developer-documentation/webhooks.html#events
+ *
+ * WARNING: In the worst case scenario that C.io changes an event
+ * name without letting us know, this would be the place to fix routing issues.
+ */
+config.events = {
+  /**
+   * This is a catch all "generic" events that we will fallback to if we don't have a specific
+   * C.io event config here.
+   */
+  generic: {
+    routingKey: '*.event.quasar',
+  },
+  email_unsubscribed: {
+    routingKey: 'email-unsubscribed.event.quasar',
+  },
+};
+
+module.exports = config;
