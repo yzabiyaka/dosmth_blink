387726c307ef92bf63d153c15bcfe65f1820af28
Rafa <rapala61@gmail.com>
2018-08-27 16:36:27 -0500

    added gambit conversations encoding key comment

--- config/workers/lib/helpers/gambit-conversations.js
+++ config/workers/lib/helpers/gambit-conversations.js
@@ -2,6 +2,7 @@
 
 const config = {
   baseURL: process.env.GAMBIT_CONVERSATIONS_API_BASE_URL || 'http://localhost:5100/api/v2',
+  // Base64 encoded key puppet:totallysecret
   apiKey: process.env.GAMBIT_CONVERSATIONS_API_KEY || 'dG90YWxseXNlY3JldA==',
   broadcastSpeedLimit: process.env.GAMBIT_BROADCAST_SPEED_LIMIT || 50,
 };
